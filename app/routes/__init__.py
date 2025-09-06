604a79b0db28f6fb9e46e52824635485dee84048
s2422092 <2422092@stu.musashino-u.ac.jp>
2025-09-06 16:57:30 +0900

    多分基本的なページ移動は可能になった

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -7,11 +7,15 @@ def create_app():
     from app.routes.home import home_bp
     from app.routes.photograph import photograph_bp
     from app.routes.personal_page import personal_page_bp
+    from app.routes.subfunction import subfunction_bp
+    from app.routes.others import others_bp
 
     
     app.register_blueprint(index_bp)
     app.register_blueprint(home_bp)
     app.register_blueprint(photograph_bp)
+    app.register_blueprint(subfunction_bp)
     app.register_blueprint(personal_page_bp)
+    app.register_blueprint(others_bp)
 
     return app
