0f441fb12075c2c98cf0ea158d8616c7cebd24b3
keikon0910 <s2422012@stu.musashino-u.ac.jp>
2025-09-06 12:16:52 +0900

    ルート直下のroutesファイルを削除

--- routes/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-def create_app():
-    app = Flask(__name__)
-
-    from routes.index import index_bp
-    app.register_blueprint(index_bp)    
-
-    return app
--- routes/home.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-home_bp = Blueprint('home', __name__, url_prefix='/', template_folder='../templates')
-
-@home_bp.route('/home')
-def home():
-    return render_template('home/home.html')
