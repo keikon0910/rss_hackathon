e3add246e267a78939e862070ee160c12c7e67ee
s2422092 <2422092@stu.musashino-u.ac.jp>
2025-09-06 13:32:19 +0900

    一旦簡単にデプロイできるようになった

--- app/routes/index.py
+++ app/routes/index.py
@@ -1,16 +1,20 @@
 from flask import Blueprint, render_template
+import os
 
-index_bp = Blueprint('index',__name__,url_prefix='/',template_folder='../templates' )
-
+index_bp = Blueprint(
+    'index',
+    __name__,
+    template_folder='../../templates/index'  # Blueprint 用にサブフォルダを指定
+)
 
 @index_bp.route('/')
 def home():
-    return render_template('index/index.html')
+    return render_template('index.html')  # ← ここではファイル名だけでOK
 
 @index_bp.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('index/login.html')
+    return render_template('login.html')
 
 @index_bp.route('/registration', methods=['GET', 'POST'])
 def registration():
-    return render_template('index/registration.html')
+    return render_template('registration.html')
