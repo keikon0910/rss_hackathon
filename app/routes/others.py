28b045507434e6dd8f624e1d13393eac4991d07a
s2422092 <2422092@stu.musashino-u.ac.jp>
2025-09-06 17:28:34 +0900

    sessionの機能を全ページにとりつければいいのですか？

--- app/routes/others.py
+++ app/routes/others.py
@@ -1,11 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template , session, redirect, url_for, flash
 
 others_bp = Blueprint('others',__name__,url_prefix='/others',template_folder='../templates' )
 
 others_bp.route('/others_personal')
 def others_personal():
+    if 'user_id' not in session:
+        flash("ログインが必要です", "error")
+        return redirect(url_for('index.login'))
     return render_template('others/others_personal.html')
 
 others_bp.route('/others_post')
 def others_post():
+    if 'user_id' not in session:
+        flash("ログインが必要です", "error")
+        return redirect(url_for('index.login'))
     return render_template('others/others_post.html')
--- app/routes/subfunction.py
+++ app/routes/subfunction.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template , session, redirect, url_for, flash
 
 subfunction_bp = Blueprint(
     'subfunction',
@@ -10,12 +10,21 @@ subfunction_bp = Blueprint(
 
 @subfunction_bp.route('/DM')
 def DM():
+    if 'user_id' not in session:
+        flash("ログインが必要です", "error")
+        return redirect(url_for('index.login'))
     return render_template('DM.html')
 
 @subfunction_bp.route('/DM_personal')
 def DM_personal():
+    if 'user_id' not in session:
+        flash("ログインが必要です", "error")
+        return redirect(url_for('index.login'))
     return render_template('DM_personal.html')
 
 @subfunction_bp.route('/notification')
 def notification():
+    if 'user_id' not in session:
+        flash("ログインが必要です", "error")
+        return redirect(url_for('index.login'))
     return render_template('notification.html')
