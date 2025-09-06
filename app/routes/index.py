eb8ecfbd55707155066ed0db419a1c89f0d82e78
s2422092 <2422092@stu.musashino-u.ac.jp>
2025-09-06 12:37:28 +0900

    消えたファイルの復活

---
+++ app/routes/index.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, render_template
+
+index_bp = Blueprint('index',__name__,url_prefix='/',template_folder='../templates' )
+
+
+@index_bp.route('/')
+def home():
+    return render_template('index/index.html')
+
+@index_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('index/login.html')
+
+@index_bp.route('/registration', methods=['GET', 'POST'])
+def registration():
+    return render_template('index/registration.html')
