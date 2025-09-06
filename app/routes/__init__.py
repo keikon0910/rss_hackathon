14817bd2d2abaf67e368aacb1e72787be3d7ad66
keikon0910 <s2422012@stu.musashino-u.ac.jp>
2025-09-06 18:40:12 +0900

    initの変更

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,22 +1,3 @@
-# from flask import Flask
-
-# def create_app():
-#     app = Flask(__name__)
-
-#     from app.routes.index import index_bp   
-#     from app.routes.home import home_bp
-#     from app.routes.photograph import photograph_bp
-#     from app.routes.personal_page import personal_page_bp
-
-#     app.register_blueprint(index_bp)
-#     app.register_blueprint(home_bp)
-#     app.register_blueprint(photograph_bp)
-#     app.register_blueprint(personal_page_bp)
-
-#     return app
-
-
-# Flask は import しない！アプリも作らない！
 from .index import index_bp
 from .home import home_bp
 from .photograph import photograph_bp
