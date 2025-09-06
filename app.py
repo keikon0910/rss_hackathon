bc738005b039616deb7f6db8c8ba66097efee240
keikon0910 <s2422012@stu.musashino-u.ac.jp>
2025-09-06 16:57:26 +0900

    3ファイルの誤りを訂正

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-from app.routes import create_app   
+from app import create_app
 
 app = create_app()
-
-app.secret_key = 'your_secret_key_here'
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,17 +1,30 @@
-from flask import Flask
+# from flask import Flask
 
-def create_app():
-    app = Flask(__name__)
+# def create_app():
+#     app = Flask(__name__)
 
-    from app.routes.index import index_bp   
-    from app.routes.home import home_bp
-    from app.routes.photograph import photograph_bp
-    from app.routes.personal_page import personal_page_bp
+#     from app.routes.index import index_bp   
+#     from app.routes.home import home_bp
+#     from app.routes.photograph import photograph_bp
+#     from app.routes.personal_page import personal_page_bp
 
-    
-    app.register_blueprint(index_bp)
-    app.register_blueprint(home_bp)
-    app.register_blueprint(photograph_bp)
-    app.register_blueprint(personal_page_bp)
+#     app.register_blueprint(index_bp)
+#     app.register_blueprint(home_bp)
+#     app.register_blueprint(photograph_bp)
+#     app.register_blueprint(personal_page_bp)
 
-    return app
+#     return app
+
+
+# Flask は import しない！アプリも作らない！
+from .index import index_bp
+from .home import home_bp
+from .photograph import photograph_bp
+from .personal_page import personal_page_bp
+
+__all__ = [
+    "index_bp",
+    "home_bp",
+    "photograph_bp",
+    "personal_page_bp",
+]
