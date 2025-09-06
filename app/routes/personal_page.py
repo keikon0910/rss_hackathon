a4858cf559883bfe272bff4033f8980ff58d3827
s2422092 <2422092@stu.musashino-u.ac.jp>
2025-09-06 15:43:41 +0900

    順調順調

--- app/routes/personal_page.py
+++ app/routes/personal_page.py
@@ -1,12 +1,17 @@
 from flask import Blueprint, render_template
 
-personal_page_bp = Blueprint('personal_page',__name__,url_prefix='/personal_page',template_folder='../templates' )
 
+personal_page_bp = Blueprint(
+    'personal_page',
+    __name__,
+    template_folder='../../templates/personal_page',
+    url_prefix=''
+)
 
 @personal_page_bp.route('/personal_page')
 def personal_page():
-    return render_template('personal_page/personal_page.html')
+    return render_template('personal_page.html')
 
 @personal_page_bp.route('/personal_setting')
 def personal_setting():
-    return render_template('personal_page/personal_setting.html')
+    return render_template('personal_setting.html')
