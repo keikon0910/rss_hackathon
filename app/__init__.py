8880e35664e422e0b20f2972d831b4fb34d060b5
s2422092 <2422092@stu.musashino-u.ac.jp>
2025-09-06 17:03:41 +0900

    コミット

--- app/__init__.py
+++ app/__init__.py
@@ -9,11 +9,15 @@ def create_app():
     from .routes.home import home_bp
     from .routes.photograph import photograph_bp
     from .routes.personal_page import personal_page_bp
+    from .routes.subfunction import subfunction_bp
+    from .routes.others import others_bp
 
     app.register_blueprint(index_bp)
     app.register_blueprint(home_bp)
     app.register_blueprint(photograph_bp)
     app.register_blueprint(personal_page_bp)
+    app.register_blueprint(subfunction_bp)
+    app.register_blueprint(others_bp)
 
     return app
 
